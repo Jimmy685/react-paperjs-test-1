de997d87b3750d6350b6297c813e5a07e3873edc
e <2182620965@qq.com>
2020-03-06 20:54:10 +0800

    2019-03-06 由于撤退次数较少，选用暴力解决

--- src/Tool/ToolMenu.tsx
+++ src/Tool/ToolMenu.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import * as paper from "paper"
-import { Button, Icon, Layout, Tooltip } from 'antd';
+import {Button, Icon, Layout, Tooltip} from 'antd';
+import {PageChange, Redo, Undo} from './RedoAndUndo'
+
 const IconFont = Icon.createFromIconfontCN({
     scriptUrl: '//at.alicdn.com/t/font_1638575_i23xlb5oazk.js',
 });
-const { Sider } = Layout;
+const {Sider} = Layout;
 
 const ToolMenu = () => {
     /**
@@ -20,9 +22,9 @@ const ToolMenu = () => {
         }
     }
     /**
-    * name ToolDrawCircle
-    * desc 画圆
-    */
+     * name ToolDrawCircle
+     * desc 画圆
+     */
     const ToolDrawCircle = () => {
         RemoveTool()
         let tool: paper.Tool = new paper.Tool()
@@ -36,11 +38,12 @@ const ToolMenu = () => {
         }
     }
     /**
-    * name ToolDrawRect
-    * desc 画方
-    */
+     * name ToolDrawRect
+     * desc 画方
+     */
     const ToolDrawRect = () => {
         RemoveTool()
+        PageChange()
         let tool: paper.Tool = new paper.Tool()
         tool.onMouseDrag = (event: paper.ToolEvent) => {
             let path: paper.Path.Rectangle = new paper.Path.Rectangle({
@@ -52,9 +55,9 @@ const ToolMenu = () => {
         }
     }
     /**
-    * name ToolFreePen
-    * desc 画笔
-    */
+     * name ToolFreePen
+     * desc 画笔
+     */
     const ToolFreePen = () => {
         RemoveTool()
         let tool: paper.Tool = new paper.Tool()
@@ -75,9 +78,9 @@ const ToolMenu = () => {
     }
 
     /**
-    * name ToolDrawSegment
-    * desc 画线段
-    */
+     * name ToolDrawSegment
+     * desc 画线段
+     */
     const ToolDrawSegment = () => {
         RemoveTool()
         let tool: paper.Tool = new paper.Tool()
@@ -90,9 +93,9 @@ const ToolMenu = () => {
         }
     }
     /**
-    * name ToolPointText
-    * desc 写字
-    */
+     * name ToolPointText
+     * desc 写字
+     */
     const ToolPointText = () => {
         RemoveTool()
         let tool: paper.Tool = new paper.Tool()
@@ -107,89 +110,89 @@ const ToolMenu = () => {
             });
         }
     }
-   //递归遍历layer树
-    const Ergodic:Function=(nowLayer:paper.Layer,point:paper.Point)=>{
-        let shape:paper.Path=null;
-        let foundIt:boolean=false;
-        let layerCache=[]
+    //递归遍历layer树
+    const Ergodic: Function = (nowLayer: paper.Layer, point: paper.Point) => {
+        let shape: paper.Path = null;
+        let foundIt: boolean = false;
+        let layerCache = []
         layerCache.push(nowLayer)
-        while(true){
+        while (true) {
             //假设layer之间没有循环嵌套
-            if(layerCache.length==0){
+            if (layerCache.length == 0) {
                 return shape;
             }
-            nowLayer=layerCache.pop()
-            nowLayer.children.forEach((item:paper.Path)=>{
-                if(item.className=='Layer'){
+            nowLayer = layerCache.pop()
+            nowLayer.children.forEach((item: paper.Path) => {
+                if (item.className == 'Layer') {
                     layerCache.push(item)
-                }else{
-                    item.bounds.selected=false;
-                    if(item.closed==false && !foundIt){
-                        if(item.segments.length==2){
+                } else {
+                    item.bounds.selected = false;
+                    if (item.closed == false && !foundIt) {
+                        if (item.segments.length == 2) {
                             //直线检测算法
-                            foundIt=point.subtract(item.segments[0].point).angle.toFixed(0)==item.segments[1].point.subtract(item.segments[0].point).angle.toFixed(0)?true:false;
-                            item.bounds.selected=foundIt;
-                            shape=foundIt?item:null;
-                        }else if(item.contains(point) && !foundIt){
+                            foundIt = point.subtract(item.segments[0].point).angle.toFixed(0) == item.segments[1].point.subtract(item.segments[0].point).angle.toFixed(0) ? true : false;
+                            item.bounds.selected = foundIt;
+                            shape = foundIt ? item : null;
+                        } else if (item.contains(point) && !foundIt) {
                             //曲线检测
-                            item.bounds.selected=!foundIt;//这条语句和foundIt=true先后顺序不能调换
-                            shape=item
-                            foundIt=true;
+                            item.bounds.selected = !foundIt;//这条语句和foundIt=true先后顺序不能调换
+                            shape = item
+                            foundIt = true;
                         }
-                    }else if(item.contains(point) && !foundIt){//判断是否包含该点（Line的contains得重写）
-                        item.bounds.selected=!foundIt;//这条语句和foundIt=true先后顺序不能调换
-                        shape=item
-                        foundIt=true;
+                    } else if (item.contains(point) && !foundIt) {//判断是否包含该点（Line的contains得重写）
+                        item.bounds.selected = !foundIt;//这条语句和foundIt=true先后顺序不能调换
+                        shape = item
+                        foundIt = true;
                     }
                 }
             })
         }
     }
     //点击边角检测(待改进)
-    const ClickCorner=(event:paper.ToolEvent,selectedShape:paper.Path)=>{
-        if(selectedShape!=null){
-            let inBottomLeft=event.point.getDistance(selectedShape.bounds.bottomLeft)>8?false:true//这个8得根据图形来调整，太小则随便点都中，太大则不好点中
-            let inBottomRight=event.point.getDistance(selectedShape.bounds.bottomRight)>8?false:true
-            let inTopLeft=event.point.getDistance(selectedShape.bounds.topLeft)>8?false:true
-            let inTopRight=event.point.getDistance(selectedShape.bounds.topRight)>8?false:true
+    const ClickCorner = (event: paper.ToolEvent, selectedShape: paper.Path) => {
+        if (selectedShape != null) {
+            let inBottomLeft = event.point.getDistance(selectedShape.bounds.bottomLeft) > 8 ? false : true//这个8得根据图形来调整，太小则随便点都中，太大则不好点中
+            let inBottomRight = event.point.getDistance(selectedShape.bounds.bottomRight) > 8 ? false : true
+            let inTopLeft = event.point.getDistance(selectedShape.bounds.topLeft) > 8 ? false : true
+            let inTopRight = event.point.getDistance(selectedShape.bounds.topRight) > 8 ? false : true
             return inBottomLeft || inBottomRight || inTopLeft || inTopRight;
         }
         return false;
     }
     //点击边界检测(待改进)
-    const ClickBounds=(event:paper.ToolEvent,selectedShape:paper.Path)=>{
-        if(selectedShape!=null){
-            let xTest=(event.point.x>selectedShape.bounds.left && event.point.x<selectedShape.bounds.right)?true:false;
-            let yTest=(event.point.y>selectedShape.bounds.top && event.point.y<selectedShape.bounds.bottom)?true:false;
+    const ClickBounds = (event: paper.ToolEvent, selectedShape: paper.Path) => {
+        if (selectedShape != null) {
+            let xTest = (event.point.x > selectedShape.bounds.left && event.point.x < selectedShape.bounds.right) ? true : false;
+            let yTest = (event.point.y > selectedShape.bounds.top && event.point.y < selectedShape.bounds.bottom) ? true : false;
             return xTest && yTest;
         }
         return false;
     }
     /**
-    * name ToolSelectPath
-    * desc 选中路径并编辑
-    */
+     * name ToolSelectPath
+     * desc 选中路径并编辑
+     */
     const ToolEditPath = () => {
         RemoveTool()
-        let tool :paper.Tool=new paper.Tool();
-        let project:paper.Project=paper.project;//这个是paper目前活跃的project，可以根据需求改成别的项目
-        let selectedShape:any=null;
+        let tool: paper.Tool = new paper.Tool();
+        let project: paper.Project = paper.project;//这个是paper目前活跃的project，可以根据需求改成别的项目
+        let selectedShape: any = null;
 
 
-        tool.onMouseDown=(event:paper.ToolEvent)=>{
-            if(!ClickBounds(event,selectedShape)){
-                selectedShape=Ergodic(project.layers[0],event.point)
+        tool.onMouseDown = (event: paper.ToolEvent) => {
+            if (!ClickBounds(event, selectedShape)) {
+                selectedShape = Ergodic(project.layers[0], event.point)
             }
         }
-        tool.onMouseDrag=(event:paper.ToolEvent)=>{
-            if(selectedShape!=null){
-                if(ClickCorner(event,selectedShape)){
-                    let x=event.point.subtract(selectedShape.bounds.center)
-                    let y=selectedShape.bounds.bottomLeft.subtract(selectedShape.bounds.center)
-                    let factor=x.divide(y).abs()
+        tool.onMouseDrag = (event: paper.ToolEvent) => {
+            if (selectedShape != null) {
+                if (ClickCorner(event, selectedShape)) {
+                    let x = event.point.subtract(selectedShape.bounds.center)
+                    let y = selectedShape.bounds.bottomLeft.subtract(selectedShape.bounds.center)
+                    let factor = x.divide(y).abs()
                     selectedShape.scale(factor)
                     //缩放
-                }else if(ClickBounds(event,selectedShape)){
+                } else if (ClickBounds(event, selectedShape)) {
                     selectedShape.translate(event.delta)
                 }
             }
@@ -199,43 +202,43 @@ const ToolMenu = () => {
      * name ToolRotate
      * desc 选中并旋转
      */
-    const ToolRotate=()=>{
+    const ToolRotate = () => {
         RemoveTool()
-        let tool :paper.Tool=new paper.Tool();
-        let project:paper.Project=paper.project;//这个是paper目前活跃的project，可以根据需求改成别的项目
-        let selectedShape:paper.Path=null;
-        let rotateFlag:boolean=false;
+        let tool: paper.Tool = new paper.Tool();
+        let project: paper.Project = paper.project;//这个是paper目前活跃的project，可以根据需求改成别的项目
+        let selectedShape: paper.Path = null;
+        let rotateFlag: boolean = false;
 
-        tool.onMouseDown=(event:paper.ToolEvent)=>{
-            if(!ClickBounds(event,selectedShape)){
-                selectedShape=Ergodic(project.layers[0],event.point)
-            }else{
-                rotateFlag=true;
+        tool.onMouseDown = (event: paper.ToolEvent) => {
+            if (!ClickBounds(event, selectedShape)) {
+                selectedShape = Ergodic(project.layers[0], event.point)
+            } else {
+                rotateFlag = true;
             }
         }
-        tool.onMouseDrag=(event:paper.ToolEvent)=>{
-            if(selectedShape!=null){
-                if(rotateFlag){
+        tool.onMouseDrag = (event: paper.ToolEvent) => {
+            if (selectedShape != null) {
+                if (rotateFlag) {
                     //旋转(到底怎么确定角度，有待进一步探索)
-                    selectedShape.rotate(event.delta.angleInRadians,selectedShape.bounds.center)
+                    selectedShape.rotate(event.delta.angleInRadians, selectedShape.bounds.center)
                 }
             }
         }
-        tool.onMouseUp=(event:paper.ToolEvent)=>{
-            rotateFlag=false;
+        tool.onMouseUp = (event: paper.ToolEvent) => {
+            rotateFlag = false;
         }
     }
 
     /**
-    * name ToolEnlarge
-    * desc 点击放大
-    */
-    const ToolEnlarge=()=>{
+     * name ToolEnlarge
+     * desc 点击放大
+     */
+    const ToolEnlarge = () => {
         RemoveTool();
-        let tool:paper.Tool=new paper.Tool();
-        tool.onMouseDown=(event:paper.ToolEvent)=>{
-            let view:paper.View=paper.view;
-            view.scale(1.5,event.point);//1.5是放大比例系数，后面的event.point是放大的中心点    
+        let tool: paper.Tool = new paper.Tool();
+        tool.onMouseDown = (event: paper.ToolEvent) => {
+            let view: paper.View = paper.view;
+            view.scale(1.5, event.point);//1.5是放大比例系数，后面的event.point是放大的中心点
         }
     }
 
@@ -251,15 +254,24 @@ const ToolMenu = () => {
     const FunctionMap = (ToolType: string) => {
         //根据类型选择对应的函数
         switch (ToolType) {
-            case 'circle': return ToolDrawCircle;
-            case 'translate': return ToolMove;
-            case 'rect': return ToolDrawRect;
-            case 'pen': return ToolFreePen;
-            case 'segment': return ToolDrawSegment;
-            case 'text': return ToolPointText;
-            case 'edit': return ToolEditPath;
-            case 'enlarge': return ToolEnlarge;
-            case 'rotate': return ToolRotate;
+            case 'circle':
+                return ToolDrawCircle;
+            case 'translate':
+                return ToolMove;
+            case 'rect':
+                return ToolDrawRect;
+            case 'pen':
+                return ToolFreePen;
+            case 'segment':
+                return ToolDrawSegment;
+            case 'text':
+                return ToolPointText;
+            case 'edit':
+                return ToolEditPath;
+            case 'enlarge':
+                return ToolEnlarge;
+            case 'rotate':
+                return ToolRotate;
         }
     }
 
@@ -267,37 +279,49 @@ const ToolMenu = () => {
         <Sider width={48} className="me-left-bar">
             <div>
                 <Tooltip placement="right" title={"pen"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("pen")} ><IconFont type="icon-pen" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("pen")}><IconFont
+                        type="icon-pen"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"segment"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("segment")}><IconFont type="icon-Line" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("segment")}><IconFont type="icon-Line"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"circle"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("circle")}><IconFont type="icon-Select-1" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("circle")}><IconFont
+                        type="icon-Select-1"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"rect"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("rect")}><IconFont type="icon-Select-" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("rect")}><IconFont type="icon-Select-"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"translate"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("translate")}><IconFont type="icon-hand" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("translate")}><IconFont
+                        type="icon-hand"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"text"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("text")}><IconFont type="icon-Text-box" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("text")}><IconFont type="icon-Text-box"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"enlarge"}>
-                    <Button type="primary" shape="circle" onClick={FunctionMap("enlarge")}><IconFont type="icon-zoom" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("enlarge")}><IconFont type="icon-zoom"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"edit"}>
-                    <Button type="primary" shape="circle"  onClick={FunctionMap("edit")}><IconFont type="icon-Link-Select" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("edit")}><IconFont
+                        type="icon-Link-Select"/></Button>
                 </Tooltip>
                 <Tooltip placement="right" title={"rotate"}>
-                    <Button type="primary" shape="circle"  onClick={FunctionMap("rotate")}><IconFont type="icon-Link-Select" /></Button>
+                    <Button type="primary" shape="circle" onClick={FunctionMap("rotate")}><IconFont
+                        type="icon-Link-Select"/></Button>
+                </Tooltip>
+                <Tooltip placement="right" title={"undo"}>
+                    <Button type="primary" shape="circle" onClick={Undo}><IconFont
+                        type="icon-zoom"/></Button>
+                </Tooltip>
+                <Tooltip placement="right" title={"redo"}>
+                    <Button type="primary" shape="circle" onClick={Redo}><IconFont
+                        type="icon-zoom"/></Button>
                 </Tooltip>
             </div>
         </Sider>
     )
 }
-
 
 
 export default ToolMenu;
